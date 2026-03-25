275d0e05a2fac4865cf14861963e84b07709b81d
Prabhanshu Pandey <prabhanshu.saciva@gmail.com>
2026-03-25 22:50:05 +0530

    fixed smtp

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+    family: 4
 });
 const express = require("express");
 const cors = require("cors");
